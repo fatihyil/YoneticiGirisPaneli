69e4167cc4ea56d6d208e02a2b6cd1a0f4b790a9
fatihyılmaz <fatihksks_@hotmail.com>
2019-07-21 21:50:43 +0300

    Proje Yüklendi

---
+++ HakTekrar/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace HakTekrar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.lblRandomSayi = new System.Windows.Forms.Label();
+            this.txtGiris = new System.Windows.Forms.TextBox();
+            this.lblKalanHak = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.Color.Black;
+            this.btnGiris.Font = new System.Drawing.Font("Adobe Fangsong Std R", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGiris.ForeColor = System.Drawing.Color.Olive;
+            this.btnGiris.Location = new System.Drawing.Point(190, 128);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(99, 35);
+            this.btnGiris.TabIndex = 0;
+            this.btnGiris.Text = "GİRİŞ";
+            this.btnGiris.UseVisualStyleBackColor = false;
+            this.btnGiris.Click += new System.EventHandler(this.BtnGiris_Click);
+            // 
+            // lblRandomSayi
+            // 
+            this.lblRandomSayi.AutoSize = true;
+            this.lblRandomSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRandomSayi.ForeColor = System.Drawing.Color.Olive;
+            this.lblRandomSayi.Location = new System.Drawing.Point(186, 60);
+            this.lblRandomSayi.Name = "lblRandomSayi";
+            this.lblRandomSayi.Size = new System.Drawing.Size(65, 20);
+            this.lblRandomSayi.TabIndex = 1;
+            this.lblRandomSayi.Text = "********";
+            // 
+            // txtGiris
+            // 
+            this.txtGiris.BackColor = System.Drawing.Color.Black;
+            this.txtGiris.ForeColor = System.Drawing.Color.Olive;
+            this.txtGiris.Location = new System.Drawing.Point(190, 102);
+            this.txtGiris.Name = "txtGiris";
+            this.txtGiris.Size = new System.Drawing.Size(190, 20);
+            this.txtGiris.TabIndex = 2;
+            // 
+            // lblKalanHak
+            // 
+            this.lblKalanHak.AutoSize = true;
+            this.lblKalanHak.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKalanHak.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblKalanHak.Location = new System.Drawing.Point(476, 135);
+            this.lblKalanHak.Name = "lblKalanHak";
+            this.lblKalanHak.Size = new System.Drawing.Size(18, 20);
+            this.lblKalanHak.TabIndex = 3;
+            this.lblKalanHak.Text = "5";
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblText.Location = new System.Drawing.Point(178, 9);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(197, 24);
+            this.lblText.TabIndex = 4;
+            this.lblText.Text = "Yönetici Giriş Paneli";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Güvenlik Numarası :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(303, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kalan Hak Sayınız :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(34, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Giriş Numaranız :";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGiris;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(546, 199);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.lblKalanHak);
+            this.Controls.Add(this.txtGiris);
+            this.Controls.Add(this.lblRandomSayi);
+            this.Controls.Add(this.btnGiris);
+            this.Name = "Form1";
+            this.Text = "GİRİŞ FORMU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.Label lblRandomSayi;
+        private System.Windows.Forms.TextBox txtGiris;
+        private System.Windows.Forms.Label lblKalanHak;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ HakTekrar/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HakTekrar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int hak = 5;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            lblRandomSayi.Text = rnd.Next(10000000, 100000000).ToString();
+        }
+
+        private void BtnGiris_Click(object sender, EventArgs e)
+        {            
+            try
+            {
+                int randomGiris = int.Parse(txtGiris.Text);
+                int randomsayi = int.Parse(lblRandomSayi.Text);
+                if (randomGiris == randomsayi)
+                {
+                    txtGiris.Enabled = false;
+                    btnGiris.Enabled = false;
+                    MessageBox.Show("Giriş Başarılı !!");
+                }
+                else
+                {
+                    hak--;
+                    lblKalanHak.Text = hak.ToString();
+                    MessageBox.Show("Yanlış Giriş yaptınız Hak Sayınız :"+hak);
+                    if (hak == 0)
+                    {
+                        btnGiris.Enabled = false;
+                        txtGiris.Enabled = false;
+                        lblRandomSayi.Text = "********";
+                        MessageBox.Show("Giriş Hakkınız kalmadı giriş yapamazsınız !");
+                    }
+                }
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+               
+            }
+        }
+    }
+}
